b1ad0a1a86b0fcc7d37480f4650c8d62fa3f7b3b
Macedman <ecmaceda17@gmail.com>
2025-08-02 23:05:40 +0800

    User and Task Queries

--- src/getTasks/getTasks.ts
+++ src/getTasks/getTasks.ts
@@ -2,11 +2,27 @@ import { Request, Response } from "express";
 import pool from "../config/db";
 
 export const getTasks = async (req: Request, res: Response) => {
-    try {
-        const result = await pool.query("SELECT * FROM users");
-        const users = result.rows;
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ message: "Internal server error" });
-    }
-}
+  try {
+    const result = await pool.query(`SELECT 
+    b.id AS board_id,
+    b.name AS board_name,
+    l.id AS list_id,
+    l.name AS list_name,
+    c.id AS card_id,
+    c.title AS card_title,
+    c.description,
+    c.position AS card_position,
+    c.due_date,
+    c.created_at AS card_created_at
+FROM card_assignees ca
+JOIN cards c ON ca.card_id = c.id
+JOIN lists l ON c.list_id = l.id
+JOIN boards b ON l.board_id = b.id
+WHERE ca.user_id = 1
+ORDER BY b.id, l.position, c.position;`);
+    const users = result.rows;
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- src/middlewares/login.ts
+++ src/middlewares/login.ts
@@ -6,44 +6,51 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const login = async (req: Request, res: Response) => {
+  console.log(req.body);
+  console.log(process.env.JWT_SECRET);
 
-    console.log(req.body);
-    console.log(process.env.JWT_SECRET);
+  const { email, password } = req.body;
 
-    const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({ message: "Email and Password is required" });
+  }
 
-    if (!email || !password) {
-        res.status(400).json({ message: "Email and Password is required" });
-    };
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [
+      email,
+    ]);
+    const user = result.rows[0];
 
-    try {
-        const result = await pool.query(
-            `SELECT * FROM users WHERE email = $1`,
-            [email]
-        );
-        const user = result.rows[0];
-
-        if (!user) {
-            res.status(401).json({ message: "User does not exist" });
-        }
-
-        //Compare password and hashed password
-        const match = await bcrypt.compare(password, user.password_hash);
-
-        if (!match) {
-            res.status(401).json({ message: "Password is incorrect" });
-        }
-
-        //Generate JWT token
-        const token = jwt.sign({userId: user.id, email: user.email}, 
-            process.env.JWT_SECRET as string,
-            {expiresIn: "1d"},
-        )
-
-        res.status(200).json({token});
-    } catch(error) {
-        console.log("Login Error: ", error);
-        res.status(500).json({error: "Internal Server error"});
+    if (!user) {
+      res.status(401).json({ message: "User does not exist" });
     }
 
-}
+    //Compare password and hashed password
+    const match = await bcrypt.compare(password, user.password_hash);
+
+    console.log(match);
+
+    if (!match) {
+      res.status(401).json({ message: "Password is incorrect" });
+    }
+
+    //Generate JWT token
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        full_name: user.full_name,
+      },
+    });
+  } catch (error) {
+    console.log("Login Error: ", error);
+    res.status(500).json({ error: "Internal Server error" });
+  }
+};
